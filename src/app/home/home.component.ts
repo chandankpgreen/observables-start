ea2b404ffdde011a7513b2e481118b56a4cf0cd6
Chandan Kumar <chandankpgreen@gmail.com>
2018-07-09 22:48:31 +0530

    using RxjS 6 without using the compat package

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
-import { Observer } from 'rxjs/Observer';
-import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs';
+import { Observer } from 'rxjs';
+import { Subscription, interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -17,12 +17,12 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000)
-    .map(
-      (data:number)=>{
-        return data * 2;
-      }
-    );
+    const myNumbers = interval(1000)
+      .pipe(map(
+        (data: number) => {
+          return data * 2;
+        })
+      );
     this.numbersObsSubscription = myNumbers.subscribe(
       (number: number) => {
         console.log(number);
